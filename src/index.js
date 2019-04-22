95e02f2442bd1fba43c60050ec655fadbee018ba
Nikita Bitkin <bitkin2295@gmail.com>
2019-04-22 09:02:31 +0300

    Code refactored

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import './style.css';
 import './assets/cat-news.jpg';
 
-var lastUrl = '', page = 0, newsDisplayed = 0;
+let lastUrl = '', page = 0, newsDisplayed = 0;
 
-const hideLoadBtn = () => { 
-  document.querySelector('#load-btn').style.display = 'none';
+const hide = (element) => {
+  document.querySelector(element).style.display = 'none';
 };
 
-const showLoadBtn = () => { 
-  document.querySelector('#load-btn').style.display = 'unset';
-};
-
-const showError = () => {
-  document.querySelector('#error-block').style.display = 'block';
-};
-
-const hideError = () => {
-  document.querySelector('#error-block').style.display = 'none';
+const show = (element) => {
+  document.querySelector(element).style.display = 'unset';
 };
 
 loadSources();
@@ -41,21 +33,19 @@ document.querySelector('#search-field')
     }
 });
 
+document.querySelector('#sources').addEventListener('click', (event) =>{
+  loadBy(`everything?sources=${event.target.id}&pageSize=5&page=1&`);
+});
+
 function loadSources(){
-  var url = 'https://newsapi.org/v2/sources?apiKey=5878ce9411164b5398895920c506a413';
-  var request = new Request(url);
+  const url = 'https://newsapi.org/v2/sources?apiKey=5878ce9411164b5398895920c506a413';
+  const request = new Request(url);
   fetch(request)
     .then(function(response) { return response.json(); })
     .then(function(data) {
-      for (var i = 0; i < data.sources.length; i++) {
+      for (let i = 0; i < data.sources.length; i++) {
         document.querySelector('#sources').innerHTML += '<button class="app__btn btn__sources" id="' + data.sources[i].id + '">' + data.sources[i].name + '</button>';
       }
-      Array.from(document.querySelectorAll('.btn__sources')).forEach(item => {
-        item.addEventListener('click', () => {
-          loadBy(`everything?sources=${item.id}&pageSize=5&page=1&`);
-        });
-      });
-
     });
 }
 
@@ -71,7 +61,7 @@ function createNewsItem(token, data){
 function createBlock(newsCount, data){
   const place = document.createDocumentFragment();
   const news_item = document.querySelector('#news-item-tpl');
-  for (var i = 0; i < newsCount; i++) {
+  for (let i = 0; i < newsCount; i++) {
     const item = (news_item.content) ? news_item.content.cloneNode(true).querySelector('.news__item') 
       : news_item.querySelector('.news__item').cloneNode(true);
     const child = createNewsItem(item, data[i]);
@@ -81,9 +71,9 @@ function createBlock(newsCount, data){
 }
 
 function loadBy(urlPart){
-  hideError();
-  var url = 'https://newsapi.org/v2/' + urlPart + 'apiKey=5878ce9411164b5398895920c506a413';
-  var request = new Request(url);
+  hide('#error-block');
+  const url = 'https://newsapi.org/v2/' + urlPart + 'apiKey=5878ce9411164b5398895920c506a413';
+  const request = new Request(url);
   fetch(request)
     .then(function(response) { return response.json(); })
     .then(function(data) {  
@@ -91,16 +81,17 @@ function loadBy(urlPart){
       newsBlock.innerHTML = '';
       const newsCount = data.articles.length;
       if(newsCount == 0){
-        showError();
-        hideLoadBtn();
+        show('#error-block');
+        hide('#load-btn');
         return;
       }      
       const block = createBlock(newsCount, data.articles);
       newsBlock.appendChild(block);
-      if(newsCount < 5)
-        hideLoadBtn();
-      else
-        showLoadBtn();
+      if(newsCount < 5){
+        hide('#load-btn');
+      }else{
+        show('#load-btn');
+      }
       lastUrl = url;
       page = 2;
       newsDisplayed = newsCount;
@@ -109,13 +100,13 @@ function loadBy(urlPart){
 
 function append(){
   lastUrl = lastUrl.replace(new RegExp('page=.*&'), 'page=' + page + '&');
-  var request = new Request(lastUrl);
+  const request = new Request(lastUrl);
   fetch(request)
     .then(function(response) { return response.json(); })
     .then(function(data) {
       const newsCount = data.articles.length;
       if(newsCount == 0){
-        hideLoadBtn();
+        hide('#load-btn');
         return;
       }     
       const block = createBlock(newsCount, data.articles);
@@ -123,7 +114,8 @@ function append(){
       newsBlock.appendChild(block);
       newsDisplayed += newsCount;
       page++;
-      if(newsCount < 5 || newsDisplayed == 40)
-        hideLoadBtn();
+      if(newsCount < 5 || newsDisplayed == 40){
+        hide('#load-btn');
+      }
     });
 }
